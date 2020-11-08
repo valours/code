53b771f582b46ea94d51e88b1966c96b82b78511
Valentin Le Flem <valentin@cremedelacreme.io>
2020-11-08 19:27:20 +0100

    Word exercices with tests

--- Arrays/words.test.ts
+++ Arrays/words.test.ts
@@ -1,13 +1,57 @@
+import faker from 'faker';
 import Words from './words';
 
 describe('Words', () => {
-  it('should add a note in student\'s grades', () => {
+  it('should return the first word by default', () => {
     // given
-    const words = new Words();
+    const firstWord = faker.lorem.word();
+    const secondWord = faker.lorem.word();
+    const words = new Words([firstWord, secondWord]);
 
     // when
+    const currentWord = words.getCurrent();
 
     // then
-    expect(false).toBeTruthy();
-  });
+    expect(currentWord).toEqual(firstWord);
+  })
+
+  it('should return the second word when forward method is call', () => {
+    // given
+    const firstWord = faker.lorem.word();
+    const secondWord = faker.lorem.word();
+    const words = new Words([firstWord, secondWord]);
+
+    // when
+    const currentWord = words.forward();
+
+    // then
+    expect(currentWord).toEqual(secondWord);
+  })
+
+  it('should return the first word when forward method is call from last word', () => {
+    // given
+    const firstWord = faker.lorem.word();
+    const secondWord = faker.lorem.word();
+    const words = new Words([firstWord, secondWord]);
+
+    // when
+    words.forward();
+    const currentWord = words.forward();
+
+    // then
+    expect(currentWord).toEqual(firstWord);
+  })
+
+  it('should return the last word when backward method is call from first word', () => {
+    // given
+    const firstWord = faker.lorem.word();
+    const secondWord = faker.lorem.word();
+    const words = new Words([firstWord, secondWord]);
+
+    // when
+    const currentWord = words.backward();
+
+    // then
+    expect(currentWord).toEqual(secondWord);
+  })
 });
---
+++ Arrays/words.ts
@@ -0,0 +1,66 @@
+export type Word = string;
+
+const isEmpty = (array:any[]) => array.length === 0;
+
+const wordListIsEmptyError = 'The word list is empty';
+const indexOfFirstWord = 0;
+
+class Words {
+  private index: number = -1;
+  private words: Word[] = []; 
+
+  constructor(words: Word[] = []) {
+    this.words = words;
+    if(!isEmpty(this.words)) {
+      this.index = indexOfFirstWord;
+    }
+  }
+
+  pushWord = (word: Word): Word[] => {
+    this.words.push(word);
+
+    return this.words
+  }
+
+  getWords = (): Word[] => {
+    return this.words;
+  }
+
+  getCurrent = (): Word => {
+    return this.words[this.index];
+  } 
+
+  incrementIndex = (): number => {
+    if(this.index === this.words.length - 1 ) {
+      this.index = indexOfFirstWord;
+    } else {
+      this.index = this.index + 1;
+    }
+    return this.index;
+  } 
+
+  forward = (): Word => {
+    if(isEmpty(this.words)) {
+      throw Error(wordListIsEmptyError);
+    }
+
+    this.incrementIndex();
+    
+    return this.words[this.index];
+  }
+
+  backward = (): Word => {
+    if(isEmpty(this.words)) {
+      throw Error(wordListIsEmptyError);
+    }
+
+    if(this.index === 0) {
+      this.index = this.words.length - 1;
+    }
+
+    return this.words[this.index];
+  } 
+
+};
+
+export default Words;
